0f1a27fa560f889593839cf6e513407e7e33d1b0
Johnson Liang <johnson.liangxinzhong@gmail.com>
2017-01-11 17:51:03 +0800

    step7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
   root "jobs#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
